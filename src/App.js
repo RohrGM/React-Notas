9a2c7cc824d310845a334b63b24d2538cab1f9d2
Guilherme Rohr Maciel <rohrgm@gmail.com>
2022-02-05 11:30:58 -0300

    state das notas

--- src/App.js
+++ src/App.js
@@ -7,20 +7,28 @@ class App extends Component {
 
   constructor(){
     super();
-    this.notas = [];
+    this.state = {
+      notas: []
+    };
+
   }
 
   criarNota(titulo, texto){
     const novaNota = {titulo, texto};
-    this.notas.push(novaNota);
-    console.log(this.notas.length)
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoState = {
+      notas: novoArrayNotas
+    };
+
+    this.setState(novoState)
+
   }
 
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota = {this.criarNota.bind(this)} />
-        <ListaDeNotas notas = {this.notas}/>
+        <ListaDeNotas notas = {this.state.notas}/>
       </section>
     );
   }
